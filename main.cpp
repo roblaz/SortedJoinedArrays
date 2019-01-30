ae7547f06934a14897069d0a2816952e72c931f5
roblaz <robert.lazarev1@gmail.com>
2019-01-30 09:56:30 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -6,44 +6,22 @@
 #define FIRSTARRAY_SIZE 5
 #define SECONDARRAY_SIZE 9
 
-void joinSortedArray(int* pFirstArray, int pFirstArray_Size, int* pSecondArray, int pSecondArray_Size, int* pMergedArray);
+int JoinArrays(int* pFirstArray, int pFirstArray_Size, int* pSecondArray, int pSecondArray_Size, int* pMergedArray);
 
 using namespace std;
 
-int compare(const void * a, const void * b)
-{
-    return (*(int*)a - *(int*)b);
-}
-
 int main()
 {
-    int iFirstArray[FIRSTARRAY_SIZE] = { 4, 2, 10, 8, 9 };
-    int iSecondArray[SECONDARRAY_SIZE] = { 12, 7, 10, 8, 13, 23, 22, 30 };
+    int iFirstArray[FIRSTARRAY_SIZE] = { 1, 2, 5, 8, 9 };
+    int iSecondArray[SECONDARRAY_SIZE] = { 5, 7, 10, 15, 19, 22, 25, 30, 33 };
     int iMergedArray[sizeof(iFirstArray) / sizeof(int) + sizeof(iSecondArray) / sizeof(int)];
 
-    cout << "Merge sorted arrays\n";
-
-    cout << "\n first array rolls:";
-    for (int i = 0; i < FIRSTARRAY_SIZE; i++) {
-        qsort(iFirstArray, FIRSTARRAY_SIZE, sizeof(int), compare);
-        cout << " " << iFirstArray[i];
-    }
-
-    cout << "\n\n second array rolls:";
-    for (int i = 0; i < SECONDARRAY_SIZE; i++) {
-        qsort(iSecondArray, SECONDARRAY_SIZE, sizeof(int), compare);
-        cout << " " << iSecondArray[i];
-    }
-
-    joinSortedArray(iFirstArray, FIRSTARRAY_SIZE, iSecondArray, SECONDARRAY_SIZE, iMergedArray);
-
+    JoinArrays(iFirstArray, FIRSTARRAY_SIZE, iSecondArray, SECONDARRAY_SIZE, iMergedArray);
 }
 
-
-
-void joinSortedArray(int* pFirstArray, int pFirstArray_Size, int* pSecondArray, int pSecondArray_Size, int* pMergedArray) {
-    char chAnswer;
+int JoinArrays(int* pFirstArray, int pFirstArray_Size, int* pSecondArray, int pSecondArray_Size, int* pMergedArray) {
     int iMergedArray_Size = pFirstArray_Size + pSecondArray_Size;
+    int iTemp;
 
     for (int i = 0; i < pFirstArray_Size; i++) {
         pMergedArray[i] = pFirstArray[i];
@@ -53,23 +31,23 @@ void joinSortedArray(int* pFirstArray, int pFirstArray_Size, int* pSecondArray, 
         pMergedArray[i + pFirstArray_Size] = pSecondArray[i];
     }
 
-    do
+
+    for (int i = 0; i < iMergedArray_Size; i++)
     {
-
-        cout << "\nDo you want to see 2 sorted and joined arrays (Yes/No)? ";
-        cin >> chAnswer;
-
-        switch (toupper(chAnswer))
+        for (int j = 0; j < iMergedArray_Size - 1; j++)
         {
-            case 'Y':
-                for (int i = 0; i < iMergedArray_Size; i++) {
-                        cout << pMergedArray[i]; cout << " ";
-                    }
-                cout << endl;
-                break;
-            case 'N':
-                break;
-            default: cout << "\nPlease enter Y or N";
+            if (pMergedArray[j] > pMergedArray[j + 1])
+            {
+                iTemp = pMergedArray[j];
+                pMergedArray[j] = pMergedArray[j + 1];
+                pMergedArray[j + 1] = iTemp;
+            }
         }
-    } while (toupper(chAnswer) != 'Y' & toupper(chAnswer) != 'N');
+    }
+
+    for (int i = 0; i < iMergedArray_Size; i++) {
+        cout << pMergedArray[i] << " ";
+    }
 }
+
+
